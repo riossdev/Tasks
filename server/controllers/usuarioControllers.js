2b7abe0be7a93e25f4080806ddb152707d202f51
Juan Rios <jriossdev@gmail.com>
2023-07-07 16:45:50 -0500

    set routes

--- server/controllers/usuarioControllers.js
+++ server/controllers/usuarioControllers.js
@@ -1,6 +1,5 @@
 import  pool  from "../db.js"
 
-
 const getTasks = (req, res)=>{  
   res.send('Getting Tasks')
 }
@@ -9,16 +8,19 @@ const getTask = (req, res)=>{
   res.send('Get task')
 }
 
-
+// create taks
 const createTask = async (req, res)=>{
-  const { title, descriptions } = req.body
-  const result = await pool.query('INSERT INTO task (title, descriptions ) VAlUES (?,?)',
-  [
-    title, 
-    descriptions
-  ])
-  console.log(result)
-  res.send('Creating Taks')
+  const { title, descriptions } = req.body;
+  const result = await pool.query(
+    "INSERT INTO task ( title, descriptions ) VAlUES (?,?)",
+  [title, descriptions]
+  );
+  // console.log(result)
+  res.json({
+    id: result.insertId,
+    title,
+    descriptions,
+  })
 }
 
 
@@ -28,6 +30,7 @@ const updateTask = (req, res)=>{
 const deleteTask = (req, res)=>{
   res.send('Delete tasks')
 }
+
 
 export {
   getTasks,
